26c77741ad8ddb51c407071d2d403b47e8c9ecc4
Hatice AKGÜL <https://github.com/haticekatranciakgul/>
2024-07-16 14:34:15 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Chart from './components/Chart';
+import Dashboard from './components/Dashboard';
+import Deposits from './components/Deposits';
+import Orders from './components/Orders';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Chart/>
+      <Dashboard/>
+      <Deposits/>
+      <Orders/>
+      
     </div>
   );
 }
